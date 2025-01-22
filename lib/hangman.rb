c50125f01287f9d470c2b8ce6e9388158def55ec
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-01-22 15:46:31 +0200

    have function to get the word and function looking at the length

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,8 +1,21 @@
-def guess_word
-  words = File.readlines('google-10000-english-no-swears.txt', chomp: true)
-  filtered_words = words.select { |word| word.length >= 3 }
-  chosen_word = filtered_words.sample
+class Hangman
+  def initialize
+    @chosen_word = nil
+  end
+
+  def guess_word
+    words = File.readlines('google-10000-english-no-swears.txt', chomp: true)
+    filtered_words = words.select { |word| word.length >= 3 }
+    @chosen_word = filtered_words.sample
+  end
   
-  # puts chosen_word
+  def get_word
+    @chosen_word
+  end
+
+
+  def word_length
+    @chosen_word.length
+  end
 end
 
